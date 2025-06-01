273c94467110efc0235818866f88e5a513858fb2
green <obviouslygreen.official@gmail.com>
2025-06-01 13:46:35 +0200

    Replace loop for registering lib functions with luaL_register

--- ffi/src/ffi.cpp
+++ ffi/src/ffi.cpp
@@ -20,16 +20,7 @@ int luaopen_ffi(lua_State* L)
 
 int luteopen_ffi(lua_State* L)
 {
-    lua_createtable(L, 0, std::size(ffi::lib) - 1);
-
-    for (auto& [name, func] : ffi::lib)
-    {
-        if (!name || !func)
-            break;
-
-        lua_pushcfunction(L, func, name);
-        lua_setfield(L, -2, name);
-    }
+    luaL_register(L, nullptr, ffi::lib);
 
     lua_setreadonly(L, -1, 1);
 
